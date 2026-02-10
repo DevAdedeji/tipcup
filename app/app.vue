b2cedbc702ed3f0bf96bb06f23a3732d598f99f8
DevAdedeji <adedejitewogbade2@gmail.com>
2026-02-10 17:37:59 +0100

    feat(analytics): added vercel analytics

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
+    <Analytics/>
     <NuxtLayout>
       <NuxtPage />
       <Toast />
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
